07a4d49f9df1f5da589849434df0982923b8ba2b
K-nours <guiboor@hotmail.com>
2026-08-12 14:45:05 +0200

    init jellyfin install

---
+++ jellyfin/install_jellyfin.sh
@@ -0,0 +1,9 @@
+# TODO
+# Create folder structure in /srv/nass/dockers/jellyfin
+# Run docker-compose up -d
+# NEXT
+# configure DuckDNS to point to the server
+# configure Nginx to reverse proxy to Jellyfin
+
+
+docker compose up -d
